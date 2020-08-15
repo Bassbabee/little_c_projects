25b7c01da40e34ec1501d79b98ae0ac165c8bc72
Maxim Ilyin <makcim510@mail.ru>
2020-08-15 19:39:20 +0300

    polymorphism

---
+++ polymorphism/animal_p.h
@@ -0,0 +1,11 @@
+#ifndef ANIMAL_P_H
+#define ANIMAL_P_H
+
+typedef void (*sound_func_t)(void*);
+
+typedef struct {
+	char* name;
+	sound_func_t sound_func;
+} animal_t;
+
+#endif
---
+++ polymorphism/cat.h
@@ -0,0 +1,12 @@
+#ifndef CAT_H
+#define CAT_H
+
+struct cat_t;
+
+struct cat_t* cat_new();
+
+void cat_ctor(struct cat_t*);
+
+void cat_dtor(struct cat_t*);
+
+#endif
